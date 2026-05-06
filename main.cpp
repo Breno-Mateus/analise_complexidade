2f72b120f19aa88bc8d17c7ca5b07429ec198ddb
Breno-Mateus <brenomateus36@gmail.com>
2026-05-06 16:08:40 -0300

    melhorando a função verificarCrescimento

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@ int main() {
 
   realizarExperimento(insertionSort, "InsertionSort", tamanhos, 10);
   realizarExperimento(selectionSort, "SelectionSort", tamanhos, 10);
-  realizarExperimento(mergeSort, "MergeSort", tamanhos, 10);
 
   return 0;
 }
